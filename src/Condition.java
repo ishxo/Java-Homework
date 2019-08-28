8248d1ae230b5894bef9720182d168a53dfe454c
ishxo <ishkhankostanyan@gmail.com>
2019-08-28 17:32:03 +0400

    condition Task 1 changes first if statement

--- src/Condition.java
+++ src/Condition.java
@@ -6,12 +6,14 @@ public class Condition {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
         int num = scanner.nextInt();
-
-        if (num > 0 & num % 2 == 0) {
-            System.out.println("Число четное " + num);
-        }  else if (num > 0 & num % 2 == 1){
-            System.out.println("Число не четное " + num);
-        } else {
+        if (num > 0) {
+            if (num % 2 == 0) {
+                System.out.println("Число четное " + num);
+            }  else if (num % 2 == 1){
+                System.out.println("Число не четное " + num);
+            }
+        }
+        else {
             System.out.println("Введите натуральное  число");
         }
         scanner.close();
