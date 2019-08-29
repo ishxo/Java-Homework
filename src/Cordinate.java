6fc339d04eec2b1e5692dd4e7337cb7befe88fbf
ishxo <ishkhankostanyan@gmail.com>
2019-08-29 11:34:28 +0400

    Cordinate-Task-3 correct

--- src/Cordinate.java
+++ src/Cordinate.java
@@ -9,7 +9,10 @@ public class Cordinate {
         double y = scanner.nextDouble();
 
         if (x <= 1 && y <= 1) {
-            if (y >= x && y <= (-1 * x) && y <= x && y >= (-1 * x)) {
+            if (y >= 0 && y >= x && y >= -x ) {
+                System.out.println("Точка попадает в заштрихованную область");
+            }
+             else if (y <= 0 && y <= x && y <= -x) {
                 System.out.println("Точка попадает в заштрихованную область");
             } else {
                 System.out.println("Точка не попадает в заштрихованную область");
