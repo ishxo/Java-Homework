7d901457a97d08638cf78f79e0d9c345a3495a07
ishxo <ishkhankostanyan@gmail.com>
2019-09-02 23:51:26 +0400

    Lesson1-Task1

--- src/Condition.java
+++ src/Condition.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
 public class Condition {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число");
