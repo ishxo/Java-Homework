3fcce93a0a3a7c3bef1773144c7c9746a9fe7ca0
ishxo <ishkhankostanyan@gmail.com>
2019-09-13 21:04:06 +0400

    Additional Task-1 implementation

---
+++ src/MinDigitCount.java
@@ -0,0 +1,19 @@
+public class MinDigitCount {
+    public static void main(String[] args) {
+        int number = 520103050;
+        int minimum = number % 10;
+        int count = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            number = (number - digit) / 10;
+            if (digit < minimum) {
+                minimum = digit;
+                count = 0;
+            }
+            if (digit == minimum) {
+                count++;
+            }
+        }
+        System.out.println("минимальная цифра встречается " + count + " раза");
+    }
+}
